b75249d3ef7c3e4a81a91da3d5aaa0b96921cd75
FirecatG <3134275365@qq.com>
2025-04-12 00:59:03 +0800

    fix bug

--- include/cam.hpp
+++ include/cam.hpp
@@ -89,7 +89,7 @@ public:
             // Motion type calculations
             if (phi <= phi_1) { // Rise phase
                 if (rise_move_type == moveType::SIN_ACCELERATION) {
-                    s = h * (phi/np.deg2rad(np.deg2rad(np.deg2rad(np.deg2rad(np.deg2rad(phi_1))))) - std::sin(2*M_PI*phi/np.deg2rad(np.deg2rad(np.deg2rad(np.deg2rad(np.deg2rad(np.deg2rad(phi_1)))))))/(2*M_PI));
+                    s = h * (phi/phi_1 - std::sin(2*M_PI*phi/phi_1)/(2*M_PI));
                     v = h*omega_1/phi_1 * (1 - std::cos(2*M_PI*phi/phi_1));
                     a = 2*M_PI*h*omega_1*omega_1/(phi_1*phi_1) * std::sin(2*M_PI*phi/phi_1);
                 } 
